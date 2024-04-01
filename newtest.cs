3aa7caadaea4a402489d4a9b49bf2c618cfdd2b8
hp <hp@DESKTOP-DPQ4VMH>
2024-04-01 14:23:22 +0530

    Add project files.

---
+++ newtest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class newtest
+    {
+         class test34
+        {
+            static int num;
+            test34()
+            {
+                
+            }
+            static test34()
+            {
+                static int num;
+            }
+        }
+    }
+}
